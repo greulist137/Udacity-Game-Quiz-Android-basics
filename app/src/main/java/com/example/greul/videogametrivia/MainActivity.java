c60f256ae03e715adb5cb0cad0bd5e569b14b031
Steven Greulich <greulich.steven@gmail.com>
2018-05-02 19:57:08 -0700

    commit - Creation of onClickListener
    The purpose of the commit is for the implementation of the following:
    1) OnClickListener in OnCreate method
    2) Shell methods for calculateScore and displayScore

--- app/src/main/java/com/example/greul/videogametrivia/MainActivity.java
+++ app/src/main/java/com/example/greul/videogametrivia/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.greul.videogametrivia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button submitQuiz = (Button) findViewById(R.id.submitButton);
+        submitQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculateScore();
+            }
+
+
+        });
+
+    }
+
+    int numberOfQuestions = 0;
+    int questionsCorrect = 0;
+
+    public void calculateScore(){
+        
+    }
+
+    public void displayScore(){
+
     }
 }
