fb3fc4d335cf86851c1be82525b87ae8dfee83b4
Steven Greulich <greulich.steven@gmail.com>
2018-05-09 18:10:22 -0700

    Commit - Answer Objects
    The purpose of this commit is for the creation of the answer objects in the Main Activity file.
    
    The next commit will be for adding logic to verify that the correct answers are selected when the user hits the SUBMIT button.

--- app/src/main/java/com/example/greul/videogametrivia/MainActivity.java
+++ app/src/main/java/com/example/greul/videogametrivia/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,8 +34,16 @@ public class MainActivity extends AppCompatActivity {
      EditText name = (EditText) findViewById(R.id.nameEntry);
      userName = name.getText().toString();
      displayScore();
-        
+
+        RadioButton q1 = (RadioButton) findViewById(R.id.Question1);
+        RadioButton q2 = (RadioButton) findViewById(R.id.Question2);
+        RadioButton q3 = (RadioButton) findViewById(R.id.Question3);
+        RadioButton q4 = (RadioButton) findViewById(R.id.Question4);
+        CheckBox q5a = (CheckBox) findViewById(R.id.Question5_1);
+        CheckBox q5b = (CheckBox) findViewById(R.id.Question5_2);
+        CheckBox q5c = (CheckBox) findViewById(R.id.Question5_3);
     }
+
 
     //The DisplayScore method will show the users score out of total questions via a Toast Message
     public void displayScore(){
